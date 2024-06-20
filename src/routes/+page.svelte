320931dd4d2fd553f44140e07958ef4674c539c8
HikariLua <88746970+HikariLua@users.noreply.github.com>
2024-06-20 20:06:45 +0000

    layout base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,68 +1,13 @@
 <script lang="ts">
 </script>
 
-<div class="flex flex-col items-center">
-  <div
-    class="flex gap-5 items-start self-stretch pr-7 w-full bg-zinc-800 max-md:flex-wrap max-md:pr-5 max-md:max-w-full"
-  >
-    <div class="flex-auto self-start max-md:max-w-full">
-      <div class="flex gap-5 max-md:flex-col max-md:gap-0">
-        <div class="flex flex-col w-[41%] max-md:ml-0 max-md:w-full">
-          <div
-            class="flex grow gap-0 text-base font-bold leading-6 text-white max-md:mt-6"
-          >
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/214aceea52363c9ce67d0093ff4f7496aaf029a6a634b70fc343a4d2b79bfacd?"
-              class="shrink-0 max-w-full aspect-[1.12] fill-white fill-opacity-0 w-[119px]"
-            />
-            <div
-              class="justify-center self-start px-6 py-3.5 mt-3.5 bg-black rounded-lg shadow-sm max-md:px-5"
-            >
-              SOBRE MIM
-            </div>
-          </div>
-        </div>
-        <div class="flex flex-col ml-5 w-[59%] max-md:ml-0 max-md:w-full">
-          <div
-            class="flex gap-5 justify-between mt-3.5 text-base font-bold leading-6 text-white whitespace-nowrap max-md:mt-9"
-          >
-            <div
-              class="justify-center px-6 py-3.5 bg-black rounded-lg shadow-sm max-md:px-5"
-            >
-              ACOLHIMENTO
-            </div>
-            <div
-              class="justify-center px-6 py-3.5 bg-black rounded-lg shadow-sm max-md:px-5"
-            >
-              INFORMAÇÕES
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="flex gap-3 items-center my-auto">
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/905d8fc839d9c455544c718be2a6cdbfbb3301d6b841f4c101bdd0dfaebbebcd?"
-        class="shrink-0 self-stretch my-auto aspect-[1.54] w-[43px]"
-      />
-      <div
-        class="shrink-0 self-stretch w-px border border-solid bg-white bg-opacity-40 border-white border-opacity-40 h-[45px]"
-      />
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/6b3dbf8b8954e94fddd11befe27228b04032015b7933bf0d6e9897efae74f5c2?"
-        class="shrink-0 self-stretch my-auto aspect-[1.54] w-[43px]"
-      />
-    </div>
-  </div>
   <div
     class="flex gap-5 justify-between mt-44 w-full max-w-[1339px] max-md:flex-wrap max-md:mt-10 max-md:max-w-full"
   >
     <div
       class="flex flex-col justify-center self-start px-5 mt-1.5 shadow-sm max-md:max-w-full"
     >
+	    
       <div
         class="text-4xl font-bold leading-10 text-yellow-400 max-md:max-w-full"
       >
@@ -116,48 +61,3 @@
       class="shrink-0 max-w-full aspect-square fill-white w-[171px]"
     />
   </div>
-  <div
-    class="flex gap-5 self-stretch px-12 py-4 mt-5 w-full bg-zinc-800 max-md:flex-wrap max-md:px-5 max-md:max-w-full"
-  >
-    <div class="flex flex-1 gap-1.5 items-center">
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/c9a3c8a4c4c1dab3f17a0c37208386ee6381f0ee15caea947031c25687cc32f2?"
-        class="shrink-0 self-stretch my-auto w-11 aspect-square"
-      /><img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/16bec0069d1f8398f922775d68d3b2979d8f66578d820eac553d65183ccc0af9?"
-        class="shrink-0 self-stretch w-14 aspect-square"
-      /><img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/8b94df52f3e0668c817482d2a3e01ba65eb9d983a258e6ef08b199a99bfc2765?"
-        class="shrink-0 self-stretch my-auto w-11 aspect-square"
-      />
-    </div>
-    <div
-      class="flex flex-1 gap-1.5 my-auto text-base font-extrabold leading-6 text-white"
-    >
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/a7a2f3506b1e34dea9c94acba1b7241712b869ebf52b818e9d8fbdfc28033562?"
-        class="shrink-0 aspect-square w-[17px]"
-      />
-      <div class="flex-auto my-auto">COPYRIGHT 2024</div>
-    </div>
-    <div class="flex flex-1 gap-1.5 items-center">
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/c9a3c8a4c4c1dab3f17a0c37208386ee6381f0ee15caea947031c25687cc32f2?"
-        class="shrink-0 self-stretch my-auto w-11 aspect-square"
-      /><img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/16bec0069d1f8398f922775d68d3b2979d8f66578d820eac553d65183ccc0af9?"
-        class="shrink-0 self-stretch w-14 aspect-square"
-      /><img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/70407c633748a81749ad767b61de727e8ad22a2dad0b0984a8f83b2d686634cc?"
-        class="shrink-0 self-stretch my-auto w-11 aspect-square"
-      />
-    </div>
-  </div>
-</div>
